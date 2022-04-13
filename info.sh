a407b6626d0e79b4176de16197eb6937114a4b98
Pawel <pkownacki90@gmail.com>
2022-04-13 14:23:02 +0200

    info.sh final ver

--- info.sh
+++ info.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-files_count = $(ls -l . | egrep -c '^-')
-dir_count = $(ls -l . | egrep -c '^d')
+files_count=$(ls -l . | egrep -c '^-')
+dir_count=$(ls -l . | egrep -c '^d')
 
-echo "Plików:" $filesCount
-echo "Katalogów:" $dirCount
+echo Plików: $files_count
+echo Katalogów: $dir_count
+if [ $files_count -eq 0 ] 
+then
+    echo Plików:
+else
+    f=$(printf "%-${files_count}s" "+")
+    echo Plików: "${f// /+}"
+fi
+if [ $dir_count -eq 0 ]
+then
+    echo Katalogów:
+else
+    d=$(printf "%-${dir_count}s" "+")
+    echo Katalogów: "${d// /+}"
+fi
