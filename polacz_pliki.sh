a1af57a9f8bf69dddaea5661a552d0d24d608880
Pawel <pkownacki90@gmail.com>
2022-05-13 12:05:27 +0200

    polacz_pliki fin

--- polacz_pliki.sh
+++ polacz_pliki.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
-if [ "$#" -lt 2 ] ; then
+
+#sprawdzanie liczy arg
+if [ "$#" -le 1 ]; then
     echo "Za mało argumentów"
     exit 0
-else
-    for file in $@
-    do
-        if [ ! -f  $file ] ; then 
-            echo "Plik nie istnieje lub nie jest plikiem zwykłym"
-            exit 0
-        elif [ ! -s $file ] ; then
-            echo "Plik jest pusty"
-            exit 0
-        else
-            echo "dalej"
-        fi
-    done
-    echo "dalej"
 fi
-
-
+#sprawdzanie pliku docelowego
+if ! [[ -f $1 ]]; then
+    echo "Plik docelowy nie istnieje lub nie jest zwykłym plikiem"
+    exit 0
+elif ! [[ -r $1 ]]; then
+    echo "Brak uprawnień do odczytu pliku docelowy"
+    exit 0
+elif ! [[ -w $1 ]]; then
+    echo "Brak uprawnień do zapisu do pliku docelowy"
+    exit 0
+fi
+#sprawdzanie plików źródłowych
+for i in "${@:2}"
+do
+    if ! [[ -f "$i" ]]; then
+        echo "Plik $i nie istnieje lub nie jest zwykłym plikiem"
+        exit 0
+    elif ! [[ -r "$i" ]]; then
+        echo "Brak uprawnień do odczytu pliku $i"
+        exit 0
+    elif ! [[ -s "$i" ]]; then
+        echo "Plik jest pusty $i"
+        exit 0
+    fi 
+done
+#łączenie plików
+cat "${@:2}" > $1
