e03756f11986e50c8d5f9d6b001199249d1275c4
Paweł <pkownacki90@gmail.com>
2022-05-13 10:02:01 +0200

    test

--- user_sprawdz.sh
+++ user_sprawdz.sh
@@ -1,7 +1,7 @@
 
 
 if id $1 &>/dev/null; then
-    last $1 -F| tail -n +1 | head -n -2 | awk '{print$1,$3,$4,$5}' | uniq -c | awk '{print $2,$3,$4,$5,$1}'
+    last $1 -RF| tail -n +1 | head -n -2 | awk '{print$1,$3,$4,$5,$7}' | uniq -c | awk '{print $2,$3,$4,$5,$6,$1}'
 else
     echo 'user not found' >&2
 fi
