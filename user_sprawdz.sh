cb034a45f2d625ce33366822ab954680508483e5
Pawel <pkownacki90@gmail.com>
2022-05-13 13:58:49 +0200

    correction user_sprawdz

--- user_sprawdz.sh
+++ user_sprawdz.sh
@@ -1,9 +1,7 @@
-
+#!/bin/bash
 
 if id $1 &>/dev/null; then
     last $1 -RF| tail -n +1 | head -n -2 | awk '{print$1,$3,$4,$5,$7}' | uniq -c | awk '{print $2,$3,$4,$5,$6,$1}'
 else
-    echo 'user not found' >&2
+    echo Podany użytkownik nie istnieje
 fi
-
-
